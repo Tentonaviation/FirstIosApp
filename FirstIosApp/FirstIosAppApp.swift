b94d7921c80e38cf9edf8aa659c6ee71bc7c3355
jgamer-playzyt <jackmorley.uk@gmail.com>
2023-05-24 14:58:59 +0100

    Initial Commit

---
+++ FirstIosApp/FirstIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIosAppApp.swift
+//  FirstIosApp
+//
+//  Created by Jack Morley on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
